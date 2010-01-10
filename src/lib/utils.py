2bb6f13e687067c7c7bb12ff6147ffb9f280b99e
Geoffrey Sneddon <geoffers@gmail.com>
2010-01-10 15:15:48 +0100

    Bump copyright year.

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -1,5 +1,5 @@
 # coding=UTF-8
-# Copyright (c) 2008-2009 Geoffrey Sneddon
+# Copyright (c) 2008-2010 Geoffrey Sneddon
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
